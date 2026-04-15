ff55672dac210fea13c8d27f8fe262fa071c8286
aschweid <aschweid@umich.edu>
2026-04-15 11:02:00 -0400

    Add counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import TestComponent from "./components/TestComponent";
+import Counter from "./components/TestComponent";
 
-const Comp = () => {
-  return <p>This is a comp component</p>
-}
 
 function App() {
   return (
     <div className="App" id='root'>
-            <TestComponent />
-
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Comp />
+        <p>Name: Alexandra Schweid</p>
+        <p>Uniquename: aschweid</p>
+
+        <Counter />
       </header>
     </div>
   );
--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,9 +1,26 @@
-const TestComponent = () => {
-  return (
-    <>
-  <h1><b>Hello!</b></h1>
-  </>
-  )
-}
+import { useState } from 'react';
 
-export default TestComponent;
+const Counter = () => {
+  // Make the stepper button behavior match the example. There are limits to how high
+  // or low the count can go.
+  // Some of the initial JSX is provided for you - not all.
+  const [count, setCount] = useState(5);
+  return (
+    <div>
+      {count ===10 ? <p>Count is too high</p>: <></>}
+      {count ===0 ? <p>Count is too low</p>: <></> }
+
+      <button
+        disabled={count === 2}
+        onClick={() => setCount(count - 1)}>-</button>
+      <output>{count}</output>
+      <button
+        disabled={count === 11}
+        onClick={() => setCount(count + 1)}
+
+      >+</button>
+    </div>
+  )
+};
+
+export default Counter;
